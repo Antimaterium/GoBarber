24f4ce187a5042d6216541007cbc36d4c85ca33e
Antimaterium <vitorafonso33@gmail.com>
2019-07-04 17:43:56 -0300

    Add User Model

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.send({message: 'Hello World'}));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Vitor Afonso',
+    email: 'vitorafonso33@gmail.com',
+    password_hash: 123,
+  });
+  res.send(user);
+});
 
 export default routes;
