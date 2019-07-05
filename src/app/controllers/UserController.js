98cf433d4f49249061128c03d4db1dedd16ac43d
Antimaterium <vitorafonso33@gmail.com>
2019-07-05 14:37:55 -0300

    Encrypting a password entry

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,9 +3,11 @@ import User from '../models/User';
 class UserController {
   static async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
+
     if (userExists) {
       return res.status(400).send({ error: 'User already exists.' });
     }
+
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({
       id,
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,11 +7,21 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      const { password } = user;
+      if (password) {
+        user.password_hash = await bcrypt.hash(password.toString(), 8);
+      }
+    });
+
+    return this;
   }
 }
 
