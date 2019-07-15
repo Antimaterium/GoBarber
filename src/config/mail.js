8ba29075f3b2e5c173e26d83f25c630497320d12
Antimaterium <vitorafonso33@gmail.com>
2019-07-15 00:32:19 -0300

    Send email - Using nodemailer to send e-mails; - Using mailtrap to send fake e-mails in dev.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '57bbdc37d0f136',
+    pass: '0f164d8d7401cf',
+  },
+  default: {
+    from: 'Vitor Afonso <noreply@gobarber.com>',
+  },
+};
